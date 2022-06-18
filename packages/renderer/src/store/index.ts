57675f6d2b034582a18a1edaa60e00763ed3e1f6
CNOliverZhang <cnoliverzhang@gmail.com>
2022-06-18 10:38:24 +0800

    feat: 存储初始化外置。

--- packages/renderer/src/store/index.ts
+++ packages/renderer/src/store/index.ts
@@ -4,8 +4,9 @@ import getSettings from './settings';
 import getArticles from './articles';
 import getThemes from './themes';
 
+const store = new Store({ watch: true });
+
 const storage = () => {
-  const store = new Store({ watch: true });
   const settings = getSettings(store);
   const articles = getArticles(store);
   const themes = getThemes(store);
